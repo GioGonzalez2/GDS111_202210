39f3bf099ccbfd7b67603642c21e1e86019e0479
Gonzalez <ggonzalez2@labs.neit.edu>
2021-10-27 17:28:15 -0400

    week4day2

---
+++ bio_part3/bio_part3.js
@@ -0,0 +1,62 @@
+var store1 =  "The Horse was Named Friday" //answer to question 1
+var store2 =  "The Post Office"            //answer to question 2
+var store3 =  "NOON"                       //answer to question 3
+var store4 =  "The letter R"
+var store5 =  "Your Name"
+var store6 =  "A Teapot"
+var store7 =  "They are grandfather, father, and son"
+var store8 =  "Ton"
+var store9 =  "A Bank"
+var store10 = "Nothing"
+
+function answer1()  {
+
+
+    document.getElementById("question1").innerHTML = store1;
+}
+
+function answer2()  {
+
+
+    document.getElementById("question2").innerHTML = store2;
+}
+function answer3()   {
+
+
+    document.getElementById("question3").innerHTML = store3;
+}
+function answer4()   {
+
+
+    document.getElementById("question4").innerHTML = store4;
+}
+function answer5()   {
+
+
+    document.getElementById("question5").innerHTML = store5;
+}
+function answer6()   {
+
+
+    document.getElementById("question6").innerHTML = store6;
+}
+function answer7()   {
+
+
+    document.getElementById("question7").innerHTML = store7;
+}
+function answer8()   {
+
+
+    document.getElementById("question8").innerHTML = store8;
+}
+function answer9()   {
+
+
+    document.getElementById("question9").innerHTML = store9;
+}
+function answer10()   {
+
+
+    document.getElementById("question10").innerHTML = store10;
+}
