4363da4f07936d65b22d46352af2fa254ecd16cf
Gonzalez <ggonzalez2@email.neit.edu>
2022-05-25 09:39:40 -0400

    week9

--- Level1/ball.js
+++
@@ -1,6 +0,0 @@
-var canvas = document.getElementById("myCanvas");
-var ctx = canvas.getContext("2d");
-ctx.fillStyle = "#FF0000";
-ctx.beginPath();
-ctx.arc(190,100,40,0,360*Math.PI/2);
-ctx.stroke();
