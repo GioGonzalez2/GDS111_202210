9c3d2294e3cbd616cdd88932f037b9d052e9995b
Gonzalez <ggonzalez2@email.neit.edu>
2022-05-26 10:39:20 -0400

    leel1-2

--- Level1/Game.js
+++ Level1/Game.js
@@ -15,7 +15,14 @@ timer = setInterval(animate, interval)
 
 
 function animate(){
-
+     //colission --->>
+     if(player.x > canvas.width - player.width/2){
+        player.vx = -player.vx; 
+    }
+    if(player.x < 0 + player.width/2){
+        player.vx = -player.vx;
+    }
+   
     context.clearRect(0,0,canvas.width, canvas.height);	
     
     player.move();
