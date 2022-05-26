c4e33e201f8387a86beee78c236fba5836d33376
Gonzalez <ggonzalez2@email.neit.edu>
2022-05-26 11:28:16 -0400

    level1boss

--- Level1/Game.js
+++ Level1/Game.js
@@ -23,18 +23,22 @@ function animate(){
     if(player.x > canvas.width - player.width/2){
         player.x = canvas.width - player.width/2
         player.vx = -player.vx; 
+        player.color = "purple";
     }
     if(player.x < 0 + player.width/2){
        player.x = 0 + player.width/2
         player.vx = -player.vx;
+        player.color = "cyan";
     }
     if(player.y > canvas.height - player.height/2){
         player.y = canvas.height - player.height/2
         player.vy = -player.vy; 
+        player.color = "magenta";
     }
     if(player.y < 0 + player.height/2){
         player.y = 0 + player.height/2
         player.vy = -player.vy;
+        player.color = "blue";
     }
 
     context.clearRect(0,0,canvas.width, canvas.height);	
